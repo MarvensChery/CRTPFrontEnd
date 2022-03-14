bbd6a71da22947f5170fd674ffefb8cd8ce21c25
Nathan Lamy <e2036136@cmaisonneuve.qc.ca>
2022-03-14 10:08:13 -0500

    modificationbanque

--- public/BanquePersonne/BanquePersonne.js
+++ public/BanquePersonne/BanquePersonne.js
@@ -1,35 +1,78 @@
 //filter + liste personne + href link
-
+let personne = null;
 let BanquePersonne = document.getElementById("BanquePersonne")
 
-let template = ``;
+async function filter() {
+    let template = ``;
 
-window.addEventListener('DOMContentLoaded', async () => {
+    let data = personne;
+
+    data = data.filter((a) => a.NomFamille.toUpperCase().includes(document.getElementById("votre-nom").value.toUpperCase()));
+        
+    data.sort(function(a, b) {
+        var textA = a.NomFamille.toUpperCase();
+        var textB = b.NomFamille.toUpperCase();
+        return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+    });
+
+    data.forEach(element => {
+        let link = "http://localhost:5000/LaRoutePourModificationDePersonne/" + element.Id;
+        template += `
+                    <tr>
+                        <td>${element.NomFamille}</td>
+                        <td>${element.Prenom1}</td>
+                        <td>${(element.Prenom2 === null) ? "" :  element.Prenom2}</td>
+                        <td>${element.Categorie}</td>
+                        <td style="border:none;"><a href='${link}'><i class="fa-solid fa-pen"></a></i></td>
+                    </tr>
+    `
+    console.log(element.Id)
+    });
+    
+    BanquePersonne.innerHTML = template;
+}
+
+async function tous() {
+    document.getElementById("votre-nom").value = ''
+
+    let template = ``;
+
+    let data = personne;
+        
+    data.sort(function(a, b) {
+        var textA = a.NomFamille.toUpperCase();
+        var textB = b.NomFamille.toUpperCase();
+        return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+    });
+
+    data.forEach(element => {
+        let link = "http://localhost:5000/LaRoutePourModificationDePersonne/" + element.Id;
+        template += `
+                    <tr>
+                        <td>${element.NomFamille}</td>
+                        <td>${element.Prenom1}</td>
+                        <td>${(element.Prenom2 === null) ? "" :  element.Prenom2}</td>
+                        <td>${element.Categorie}</td>
+                        <td style="border:none;"><a href='${link}'><i class="fa-solid fa-pen"></a></i></td>
+                    </tr>
+    `
+    console.log(element.Id)
+    });
+    
+    BanquePersonne.innerHTML = template;
+}
+
+document.getElementById("filtrer").addEventListener('click', filter);
+document.getElementById("tous").addEventListener('click', tous);
+
+async function getPersonnes() {
     let reponse = await fetch(`http://localhost:3000/banquepersonne`)
-
+    
     if (reponse.ok) {
-        const data = await reponse.json();
-        
-        data.sort(function(a, b) {
-            var textA = a.NomFamille.toUpperCase();
-            var textB = b.NomFamille.toUpperCase();
-            return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
-        });
-
-        data.forEach(element => {
-            let link = "http://localhost:5000/LaRoutePourModificationDePersonne/" + element.Id;
-            template += `
-                        <tr>
-                            <td>${element.NomFamille}</td>
-                            <td>${element.Prenom1}</td>
-                            <td>${(element.Prenom2 === null) ? "" :  element.Prenom2}</td>
-                            <td>${element.Categorie}</td>
-                            <td style="border:none; padding-right: 1000px;"><a href='${link}'><i class="fa-solid fa-pen"></a></i></td>
-                        </tr>
-        `
-        console.log(element.Id)
-        });
-        
-        BanquePersonne.innerHTML = template;
+        personne = await reponse.json();
+        console.log(personne)
+        filter();
     }
-})
+}
+getPersonnes()
+
