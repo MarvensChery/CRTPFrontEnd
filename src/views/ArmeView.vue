22abd2cc56049a94afd239c55c18bc2a90c5ea06
samy <e2072931@cmaisonneuve.qc.ca>
2022-04-20 00:54:37 -0400

    add vue file

---
+++ src/views/ArmeView.vue
@@ -0,0 +1,160 @@
+<template>
+    <div class="container mb-4 is-desktop">
+      <form id="formulaireAjouter">
+        <h1 class="has-text-black " style="height:135px; text-align:center;
+        font-size: 24px; padding-top: 5%;" ><b><u>MODIFICATION D'UNE RÉPONSE ARME À FEU</u></b></h1>
+        <br>
+        <br>
+        <div class="box">
+            <div class="columns is-centered">
+                <div class="column is-half">
+                    <div class="field">
+                        <label for="noSerie" class="label">Numéro de série</label>
+                        <div class="control has-icons-left has-icons-right">
+                            <input id="noSerie" class="input" type="text" name="noSerie"
+                            placeholder="Numéro de série" required/>
+                            <span class="icon is-small is-left">
+                                <i class="fas fa-user"></i>
+                            </span>
+                        </div>
+                        <p id="nomError" class="help is-danger is-hidden">
+                            *Champ obligatoire : seulement des lettres et '-' sont valides</p>
+                    </div>
+                    <div class="field">
+                        <label for="marque" class="label">Marque</label>
+                        <div class="control has-icons-left has-icons-right">
+                            <input id="marque" class="input" type="text" name="marque"
+                            placeholder="Marque" required/>
+                            <span class="icon is-small is-left">
+                                <i class="fas fa-user"></i>
+                            </span>
+                        </div>
+                        <p id="nomError" class="help is-danger is-hidden">
+                            *Champ obligatoire : seulement des lettres et '-' sont valides</p>
+                    </div>
+                    <div class="field">
+                        <label for="calibre" class="label">Calibre</label>
+                        <div class="control has-icons-left has-icons-right">
+                            <input id="calibre" class="input" type="text" name="calibre"
+                            placeholder="Calibre" required/>
+                            <span class="icon is-small is-left">
+                                <i class="fas fa-user"></i>
+                            </span>
+                        </div>
+                        <p id="nomError" class="help is-danger is-hidden">
+                            *Champ obligatoire : seulement des lettres et '-' sont valides</p>
+                    </div>
+                    <div class="field">
+                        <label for="typeArme" class="label">Type d'arme</label>
+                        <div class = "control">
+                            <select id="typeArme" class="select" name="typeArme" required>
+                            <option></option>
+                            <option>Révolver</option>
+                            <option>Pistolet</option>
+                            <option>Carabine</option>
+                            <option>Fusil</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div id="NoEvenement" class="columns is-mobile is-multiline is-centered">
+                        <div class="column is-2">
+                            <label class="has-text-black" for="NoEvent"><b>Numéro évenement</b>
+                                <span style="color: red">*</span></label><br><br>
+                            <select id="NoEvent" class="select" name="NoEvent" required>
+                                <option></option>
+                                <option>123</option>
+                                <option>302</option>
+                                <option>108</option>
+                            </select>
+                        </div>
+
+                        <div class="column is-1-desktop is-2-mobile">
+                            <label class="has-text-black" for="annee"><b>Année</b>
+                                <span style="color: red">*</span></label><br><br>
+                            <input id="annee" type="text" name="annee" placeholder="Année"
+                                required/>
+                        </div>
+
+                        <div class="column is-1-desktop is-2-mobile">
+                            <label class="has-text-black" for="ddm"><b>Mois</b><span
+                                style="color: red">*</span></label><br><br>
+                            <select id="Mois" class="select" name="mois" required>
+                            <option></option>
+                            <option value="01">01</option>
+                            <option value="02">02</option>
+                            <option value="03">03</option>
+                            <option value="04">04</option>
+                            <option value="05">05</option>
+                            <option value="06">06</option>
+                            <option value="07">07</option>
+                            <option value="08">08</option>
+                            <option value="09">09</option>
+                            <option value="10">10</option>
+                            <option value="11">11</option>
+                            <option value="12">12</option>
+
+                            </select>
+                        </div>
+                        <div class="column is-1-desktop is-2-mobile">
+                            <label class="has-text-black" for="ddm"><b>Jour</b><span
+                                style="color: red">*</span></label><br><br>
+                            <select id="Jour" class="select" name="jour" required>
+                            <option></option>
+                            <option>01</option>
+                            <option>02</option>
+                            <option>03</option>
+                            <option>04</option>
+                            <option>05</option>
+                            <option>06</option>
+                            <option>07</option>
+                            <option>08</option>
+                            <option>09</option>
+                            <option>10</option>
+                            <option>11</option>
+                            <option>12</option>
+                            <option>13</option>
+                            <option>14</option>
+                            <option>15</option>
+                            <option>16</option>
+                            <option>17</option>
+                            <option>18</option>
+                            <option>19</option>
+                            <option>20</option>
+                            <option>21</option>
+                            <option>22</option>
+                            <option>23</option>
+                            <option>24</option>
+                            <option>25</option>
+                            <option>26</option>
+                            <option>27</option>
+                            <option>28</option>
+                            <option>29</option>
+                            <option>30</option>
+                            <option>31</option>
+                            </select>
+                        </div>
+
+                        <div class="column is-2">
+                            <label class="has-text-black" for="NoSeq"><b>Numéro Séquentiel</b>
+                                <span style="color: red">*</span></label><br><br>
+                            <input id="NoSeq" type="text" name="NoSeq"
+                                placeholder="Numéro Séquentiel" required/>
+                        </div>`
+                   </div>
+                </div>
+            </div>
+        </div>
+      </form>
+    </div>
+</template>
+
+<script>
+
+// noinspection JSUnusedGlobalSymbols
+export default {
+    name: 'ArmeView',
+};
+</script>
+
+<style scoped>
+</style>
