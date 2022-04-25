415b76b70676987fd5068c5c44095168128d20cd
LucasMontionM9 <e1963227@cmaisonneuve.qc.ca>
2022-04-25 11:55:19 -0400

    Confirmation Supprimer Arme

--- src/views/ArmeView.vue
+++ src/views/ArmeView.vue
@@ -9,6 +9,25 @@
         </h1>
         <br>
         <br>
+        <div class='dialog-ovelay' v-if="confimation">
+            <div class='dialog'>
+                <header>
+                    <h3> confirmation </h3>
+                    <i class='fa fa-close'></i>
+                </header>
+                <div class='dialog-msg'>
+                    <p> Voulez-vous supprimer? </p>
+                </div>
+                <footer>
+                    <div class='controls'>
+                        <button class='button button-danger doAction'
+                                v-on:click="deleteArme"> oui  </button>
+                        <button class='button button-default cancelAction'
+                                v-on:click="confimation=''"> non  </button>
+                    </div>
+                </footer>
+            </div>
+        </div>
         <div class="box" v-if="arme">
                 <div class="success" v-if="sucess">
                     <a class="closebtn" href="/armes">&times;</a>
@@ -174,7 +193,7 @@
                 v-on:click="this.addArme">Ajouter</button>&nbsp;
                 <button type="reset"
                 v-if="this.$route.name === 'MarmeView'"
-                v-on:click="deleteArme">Supprimer</button>&nbsp;
+                v-on:click="confirmation">Supprimer</button>&nbsp;
                 <button type="button"
                     v-on:click="this.$router.push({ name: 'armesView' })">Annuler</button>
             </div>
@@ -215,6 +234,7 @@ export default {
             NoSerieValid: '',
             MarqueValid: '',
             CalibreValid: '',
+            confimation: '',
         };
     },
     mounted() {
@@ -223,11 +243,16 @@ export default {
         }
     },
     methods: {
+        confirmation() {
+            this.confimation = 'validation';
+        },
         async deleteArme() {
             const api = await fetch(`${svrURL}/armes/${this.$route.params.idArme}`, { method: 'DELETE' }); // Permet de delete une arme
             const res = await api.json();
-            if (res.success) this.sucess = res.message;
-            else this.error = res.message;
+            if (res.success) {
+                this.sucess = res.message;
+                this.confimation = '';
+            } else this.error = res.message;
         },
         async addArme() { // Permet de add une arme
             if (this.NoSerie === '') {
@@ -367,6 +392,85 @@ html, body {
     color: #666;
     line-height: 22px;
     }
+    .dialog-ovelay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0, 0, 0, 0.50);
+    z-index: 999999
+}
+.dialog-ovelay .dialog {
+    width: 400px;
+    margin: 100px auto 0;
+    background-color: #fff;
+    box-shadow: 0 0 20px rgba(0,0,0,.2);
+    border-radius: 3px;
+    overflow: hidden
+}
+.dialog-ovelay .dialog header {
+    padding: 10px 8px;
+    background-color: #f6f7f9;
+    border-bottom: 1px solid #e5e5e5
+}
+.dialog-ovelay .dialog header h3 {
+    font-size: 14px;
+    margin: 0;
+    color: #555;
+    display: inline-block
+}
+.dialog-ovelay .dialog header .fa-close {
+    float: right;
+    color: #c4c5c7;
+    cursor: pointer;
+    transition: all .5s ease;
+    padding: 0 2px;
+    border-radius: 1px
+}
+.dialog-ovelay .dialog header .fa-close:hover {
+    color: #b9b9b9
+}
+.dialog-ovelay .dialog header .fa-close:active {
+    box-shadow: 0 0 5px #673AB7;
+    color: #a2a2a2
+}
+.dialog-ovelay .dialog .dialog-msg {
+    padding: 12px 10px
+}
+.dialog-ovelay .dialog .dialog-msg p{
+    margin: 0;
+    font-size: 15px;
+    color: #333
+}
+.dialog-ovelay .dialog footer {
+    border-top: 1px solid #e5e5e5;
+    padding: 8px 10px
+}
+.dialog-ovelay .dialog footer .controls {
+    direction: rtl
+}
+.dialog-ovelay .dialog footer .controls .button {
+    padding: 5px 15px;
+    border-radius: 3px
+}
+.button {
+  cursor: pointer
+}
+.button-default {
+    background-color: rgb(248, 248, 248);
+    border: 1px solid rgba(204, 204, 204, 0.5);
+    color: #5D5D5D;
+}
+.button-danger {
+    background-color: #f44336;
+    border: 1px solid #d32f2f;
+    color: #f5f5f5
+}
+.link {
+  padding: 5px 10px;
+  cursor: pointer
+}
     .closebtn {
         margin-left: 15px;
         color: white;
