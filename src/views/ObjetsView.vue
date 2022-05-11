c6b12004bf51c101132d49fe7a61e21400d990ec
samy <e2072931@cmaisonneuve.qc.ca>
2022-05-10 23:10:49 -0400

    objets liste

--- src/views/ObjetsView.vue
+++ src/views/ObjetsView.vue
@@ -1,23 +1,4 @@
 <template>
-<<<<<<< 9fcbb467b451c5ed1057c18185f1c9cc59312c9c
-=======
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< HEAD
->>>>>>> Auto stash before rebase of "refs/heads/Sprint2"
-    <div v-if="this.$route.name === 'objetsView'">
-        <h1 class="has-text-black " style="height:135px; text-align:center; font-size: 24px;">
-        <b><u>LISTE D'OBJETS</u></b></h1>
-        <div class="columns has-text-right has-text-black is-centered"
-        style="padding-right: 5%; padding-bottom: 5%;">
-            <div class="select">
-                <select v-model="valeursFiltres">
-                    <option></option>
-                    <option v-for="i in optionsValeurs" :value="i" :key="i">{{i}}</option>
-                </select>
-=======
-=======
->>>>>>> Stashed changes
     <div>
         <div v-if="this.$route.name === 'objetsView'">
             <h1 class="has-text-black " style="height:135px; text-align:center; font-size: 24px;">
@@ -64,10 +45,6 @@
                     </div>
                         </a>
                     </div>
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
             </div>
         </div>
         <div v-if="this.$route.name === 'valeursView'">
@@ -96,26 +73,6 @@
                                 <th>Type d'évènement</th>
                                 <th>Numéro d'évènement</th>
                             </tr>
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-                        <tr v-for="o in filtresValeurs" v-bind:key="o.idBOB">
-                            <td>{{ o.NoSerie }}</td>
-                            <td>{{ o.Marque }}</td>
-                            <td>{{ o.Modele }}</td>
-                            <td>{{ o.TypeObjet }}</td>
-                            <td>{{ o.NoEvenement }}</td>
-                            <td>
-                                <router-link :to="{ name: 'modifObjetView',
-                                            params: { idObjet: o.IdBOB } }">
-                                <i class='fas fa-edit'></i>
-                            </router-link>
-                            </td>
-                        </tr>
-                    </table>
-                </div>
-=======
-=======
->>>>>>> Stashed changes
                             <tr v-for="v in filtresValeurs" v-bind:key="v.IdIBVA">
                                 <td>{{ v.Identifiant }}</td>
                                 <td>{{ v.Auteur }}</td>
@@ -131,45 +88,10 @@
                             </tr>
                         </table>
                     </div>
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
                     </a>
             </div>
             </div>
         </div>
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-        <div class="columns">
-            <div id="detail" class="column is-centered">
-                <a id="valeurs">
-                <div class="table-container">
-                    <table class="table has-text-black is-fullwidth" style="text-align:center;">
-                        <tr>
-                            <th>Identifiant</th>
-                            <th>Auteur</th>
-                            <th>Type de Valeur</th>
-                            <th>Type d'évènement</th>
-                            <th>Numéro d'évènement</th>
-                        </tr>
-                        <tr v-for="v in filtresValeurs" v-bind:key="v.IdIBVA">
-                            <td>{{ v.Identifiant }}</td>
-                            <td>{{ v.Auteur }}</td>
-                            <td>{{ v.TypeValeur }}</td>
-                            <td>{{ v.TypeEvenement }}</td>
-                            <td>{{ v.NoEvenement }}</td>
-                            <td>
-                                <router-link :to="{ name: 'modifValeurView',
-                                            params: { idValeur: v.IdIBVA } }">
-                                    <i class='fas fa-edit'></i>
-                                </router-link>
-                            </td>
-                        </tr>
-                    </table>
-=======
-=======
->>>>>>> Stashed changes
         <div v-if="this.$route.path === '/armes'">
             <h1 class="has-text-black " style="height:135px; text-align:center; font-size: 24px;">
             <b><u>LISTE D'ARMES</u></b></h1>
@@ -180,48 +102,10 @@
                         <option></option>
                         <option v-for="i in optionsValeurs" :value="i" :key="i">{{i}}</option>
                     </select>
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
                 </div>
                 <input id="type" style="height: 40px;" placeholder="Valeur du filtre"
                 v-model="valValeurs">
             </div>
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-            <input id="type" style="height: 40px;" placeholder="Valeur du filtre"
-            v-model="valValeurs">
-        </div>
-        <div class="columns">
-            <div id="detail" class="column is-centered">
-                <a id="armes">
-                <div class="table-container">
-                    <table class="table has-text-black is-fullwidth" style="text-align:center;">
-                        <tr>
-                            <th>Numéro de Série</th>
-                            <th>Marque</th>
-                            <th>Calibre</th>
-                            <th>Type d'armes</th>
-                            <th>Numéro d'évènement</th>
-                        </tr>
-                        <tr v-for="a in filtresValeurs" v-bind:key="a.IdIBAF">
-                            <td>{{ a.NoSerie }}</td>
-                            <td>{{ a.Marque }}</td>
-                            <td>{{ a.Calibre }}</td>
-                            <td>{{ a.TypeArme }}</td>
-                            <td>{{ a.NoEvenement }}</td>
-                            <td>
-                                <router-link :to="{ name: 'modifArmeView',
-                                            params: { idArme: a.IdIBAF } }">
-                                    <i class='fas fa-edit'></i>
-                                </router-link>
-                            </td>
-                        </tr>
-                    </table>
-=======
-=======
->>>>>>> Stashed changes
             <div class="columns">
                 <div id="detail" class="column is-centered">
                     <a id="armes">
@@ -250,10 +134,6 @@
                         </table>
                     </div>
                     </a>
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
                 </div>
             </div>
         </div>
