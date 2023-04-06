8d676fe45c61bd4000d172e8b113cc164d8289e7
Marc <mlevasseur@cmaisonneuve.qc.ca>
2023-04-06 06:04:04 -0400

    Corrections

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <MenuProfesseurView v-if="Professeur === true"/>
+        <EtudiantView v-else/>
         <HeaderView/>
         <router-view/>
     </div>
@@ -9,11 +10,12 @@
 <script>
 
 import MenuProfesseurView from '@/components/MenuProfesseurView.vue';
+import EtudiantView from '@/views/EtudiantView.vue';
 import HeaderView from '@/components/HeaderView.vue';
 
 export default {
     name: 'App',
-    components: { MenuProfesseurView, HeaderView },
+    components: { MenuProfesseurView, EtudiantView, HeaderView },
     data() {
         return {
             Professeur: true,
